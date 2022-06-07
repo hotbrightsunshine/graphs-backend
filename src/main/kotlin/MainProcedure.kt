ec65e592f80c7e325bd9a7b10779ff52b669d1d2
lai2-01 <lai2-01@itismeucci.com>
2022-06-07 12:36:50 +0200

    to fix, yet working

--- src/main/kotlin/MainProcedure.kt
+++ src/main/kotlin/MainProcedure.kt
@@ -1,3 +1,7 @@
 fun main() {
-    // SimpleWeightedGraph
+    val g = SquareGraph(3, 3);
+    println(g);
+
+    val sp = g.dijkstra();
+    println(sp.getPath("Vertex 0", "Vertex 6"));
 }
--- src/main/kotlin/SquareGraph.kt
+++ src/main/kotlin/SquareGraph.kt
@@ -1,15 +1,14 @@
 import org.jgrapht.Graph
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath
 import org.jgrapht.generate.GridGraphGenerator
-import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleWeightedGraph
 import org.jgrapht.traverse.DepthFirstIterator
 import org.jgrapht.util.SupplierUtil
 import java.util.function.Supplier
 
-class SquareGraph {
-    // Not null
-    private val rows = 0
-    private val columns = 0
+class SquareGraph(var rows: Int = 2, var columns: Int = 2){
+
     private val vertexSupplier: Supplier<String> = object : Supplier<String> {
         private var id = 0
         override fun get(): String {
@@ -18,15 +17,27 @@ class SquareGraph {
     }
 
     // Prima serve creare l'oggetto grafo
-    private val completeGraph: Graph<String, DefaultEdge> =
-        SimpleWeightedGraph(vertexSupplier, SupplierUtil.createDefaultEdgeSupplier())
+    var completeGraph: Graph<String, DefaultEdge> =
+        SimpleWeightedGraph(vertexSupplier, SupplierUtil.createDefaultEdgeSupplier());
 
     // Successivamente si crea un generatore che crea la struttura di `completeGraph`
-    private val graphGenerator = GridGraphGenerator<String, DefaultEdge>(rows, columns)
+    var graphGenerator : GridGraphGenerator<String, DefaultEdge> =
+        GridGraphGenerator<String, DefaultEdge>(rows, columns);
 
-    // Creo il grafo
-    fun initialize() {
-        graphGenerator.generateGraph(completeGraph)
+    init{
+        if(rows < 2) rows = 2;
+        if(columns < 2) columns = 2;
+        initialize();
+    }
+
+    private fun initialize() {
+        graphGenerator!!.generateGraph(completeGraph)
+    }
+
+
+    fun dijkstra() : DijkstraShortestPath<String, DefaultEdge> {
+        var dijkstra = DijkstraShortestPath(completeGraph);
+        return dijkstra;
     }
 
     override fun toString(): String {
@@ -34,7 +45,7 @@ class SquareGraph {
         val iter: Iterator<String> = DepthFirstIterator(completeGraph)
         while (iter.hasNext()) {
             val vertex = iter.next()
-            str += "Vertice `" + vertex + "` connesso a " + completeGraph.edgesOf(vertex)
+            str += "Vertice `" + vertex + "` connesso a " + completeGraph!!.edgesOf(vertex).toString() + "\n";
         }
         return str
     }
